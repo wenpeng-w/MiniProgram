f28afdc01ed752f8c6caa7e684e29e9a4781e424
wenpeng-w <449202631@qq.com>
2018-08-01 23:08:36 +0800

    story list

--- pages/story/story.js
+++ pages/story/story.js
@@ -50,6 +50,20 @@ Page({
         iconPath: './images/wujx_icon.png',
         name: '五角星'
       } 
+    ],
+    storyList: [
+      {
+        image: '//upload-images.jianshu.io/upload_images/4885411-3f620ebee1bb9882?imageMogr2/auto-orient/strip%7CimageView2/2/w/700',
+        title: '【设计】浅谈长图版式布局中的几种常用方法浅谈长图版式布局中的几种常用方法',
+        time: '2018.08.01 14:10',
+        tags: ['设计']
+      },
+      {
+        image: '//upload.jianshu.io/admin_banners/web_images/4358/a52cb0d0ef97a08087a234f4e25702b2471d49a4.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540',
+        title: '【设计】浅谈长图版式布局中的几种常用方法',
+        time: '2018.08.01 14:10',
+        tags: ['产品']
+      }
     ]
   },
 
